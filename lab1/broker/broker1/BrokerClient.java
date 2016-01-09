2f88533bdf80850fa15a5ade66be56ed38914b76
CEYeYuan <eceye.yuan@mail.utoronto.ca>
2016-01-09 17:09:57 -0500

    use the same format of sample code

--- lab1/broker/broker1/BrokerClient.java
+++ lab1/broker/broker1/BrokerClient.java
@@ -4,26 +4,50 @@ import java.util.*;
 public class BrokerClient{
 
 	public static void main(String[] args){
+		String host=null;
+		int port=-1;
 		try{
-			String host=args[0];
-			int port=Integer.parseInt(args[1]);
+			if(args.length == 2 ) {
+				host=args[0];
+				port=Integer.parseInt(args[1]);
+			} else {
+				System.err.println("ERROR: Invalid arguments!");
+				System.exit(-1);
+			}	
 			System.out.println("host: "+host+" port number: "+port);
 			Socket socket=new Socket(host, port);
 			Scanner scr = new Scanner(System.in);	
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			ObjectInputStream socket_reader=new ObjectInputStream(socket.getInputStream()); //for reading lines
-			ObjectOutputStream writer=new ObjectOutputStream(socket.getOutputStream());	
-			while(true){
-				String request=scr.next();
+			ObjectOutputStream writer=new ObjectOutputStream(socket.getOutputStream());
+
+			System.out.print("CONSOLE>");	
+			String request=null;
+			while((request = scr.next()) != null
+				&& request.toLowerCase().indexOf("bye") == -1){
 				BrokerPacket packet=new BrokerPacket();
 				packet.type=BrokerPacket.BROKER_REQUEST; 
 				packet.symbol=request;
 				writer.writeObject(packet);
 
 				BrokerPacket reply=(BrokerPacket)socket_reader.readObject();
-				System.out.println("Quote from broker: "+reply.symbol);
+				if (reply.type == BrokerPacket.BROKER_QUOTE)
+					System.out.println("Quote from broker: "+reply.symbol);
+				/* re-print console prompt */
+				System.out.print("CONSOLE>");
 
-			}	
+			}
+			/* tell server that i'm quitting */
+			BrokerPacket packetToServer = new BrokerPacket();
+			packetToServer.type = BrokerPacket.BROKER_BYE ;
+			packetToServer.symbol = "Bye!";
+			writer.writeObject(packetToServer);
+
+			writer.close();
+			socket_reader.close();
+			reader.close();
+			socket.close();
+
 		}
 		catch(Exception e){
 			e.printStackTrace();
