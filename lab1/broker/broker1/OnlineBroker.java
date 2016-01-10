ebedab3e1deca5935154bb3b69aabc2f52604d02
CEYeYuan <eceye.yuan@mail.utoronto.ca>
2016-01-09 19:27:04 -0500

    broker1 done

--- lab1/broker/broker1/OnlineBroker.java
+++ lab1/broker/broker1/OnlineBroker.java
@@ -1,10 +1,27 @@
-import java.io.*;
 import java.net.*;
-import java.util.*;
-public class OnlineBroker{
-	public static void main(String[] args){
-		int port=Integer.parseInt(args[0]);
-		System.out.println("host: "+host+" port number: "+port);
-		Socket socket=new Socket(host, port);
-	}
+import java.io.*;
+
+public class OnlineBroker {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        boolean listening = true;
+
+        try {
+        	if(args.length == 1) {
+        		serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+        	} else {
+        		System.err.println("ERROR: Invalid arguments!");
+        		System.exit(-1);
+        	}
+        } catch (IOException e) {
+            System.err.println("ERROR: Could not listen on port!");
+            System.exit(-1);
+        }
+
+        while (listening) {
+        	new BrokerServerHandlerThread(serverSocket.accept()).start();
+        }
+
+        serverSocket.close();
+    }
 }
