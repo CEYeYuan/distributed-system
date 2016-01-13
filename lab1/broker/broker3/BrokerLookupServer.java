cb4f4ca0fac46fa0e2f6d124dd8a6b44adfafdd0
CEYeYuan <eceye.yuan@mail.utoronto.ca>
2016-01-13 10:54:34 -0500

    added lookup server

--- lab1/broker/broker3/BrokerLookupServer.java
+++ lab1/broker/broker3/BrokerLookupServer.java
@@ -1,9 +1,27 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.util.concurrent.*;
 
 public class BrokerLookupServer{
-	public static void main(){
-		
+	public static void main(String[] args) throws Exception{
+		ServerSocket serverSocket = null;
+		ConcurrentHashMap<String,ArrayList<BrokerLocation>> map=new ConcurrentHashMap<String,ArrayList<BrokerLocation>>();
+		boolean listening=true;
+        try {
+        	if(args.length == 1) {
+        		serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+        	} else {
+        		System.err.println("ERROR: Invalid arguments!");
+        		System.exit(-1);
+        	}
+        } catch (IOException e) {
+            System.err.println("ERROR: Could not listen on port!");
+            System.exit(-1);
+        }
+         while (listening) {
+            new Thread(new BrokerLookupServerHandlerThread(serverSocket.accept(),map)).start();
+          
+        }
 	}
 }
---
+++ lab1/broker/broker3/BrokerLookupServerHandlerThread.java
@@ -0,0 +1,91 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.ArrayList;
+
+public class BrokerLookupServerHandlerThread implements Runnable{
+	private Socket socket = null;
+	private ConcurrentHashMap<String, ArrayList<BrokerLocation>> map=null;
+
+	public BrokerLookupServerHandlerThread(Socket socket,ConcurrentHashMap<String,ArrayList<BrokerLocation>> map) {
+		this.map=map;
+		this.socket = socket;
+		System.out.println("Created new Thread to handle server registration");
+	}
+
+	public void run() {
+
+		boolean gotByePacket = false;
+		try {
+
+			/* stream to read from client */
+			ObjectInputStream fromClient = new ObjectInputStream(socket.getInputStream());
+			BrokerPacket packetFromClient;
+			
+			/* stream to write back to client */
+			ObjectOutputStream toClient = new ObjectOutputStream(socket.getOutputStream());
+			
+
+			while (( packetFromClient = (BrokerPacket) fromClient.readObject()) != null) {
+				/* create a packet to send reply back to client */
+				BrokerPacket packetToClient = new BrokerPacket();
+
+				if(packetFromClient.type == BrokerPacket.LOOKUP_REGISTER){
+					ArrayList list=null;
+					if(map.get(packetFromClient.symbol)==null){
+						list=new ArrayList<BrokerLocation>();
+						map.put(packetFromClient.symbol,list);
+					}
+					else
+						list=map.get(packetFromClient.symbol);
+					for(int i=0;i<packetFromClient.locations.length;i++)
+						list.add(packetFromClient.locations[i]);
+					
+					/* send reply back to client */
+					packetToClient.type=BrokerPacket.LOOKUP_REPLY;
+					packetToClient.symbol=packetFromClient.symbol+ " registered";
+					System.out.println(packetFromClient.symbol+ " registered");
+					toClient.writeObject(packetToClient);
+					
+					/* wait for next packet */
+					continue;
+				}
+
+				if(packetFromClient.type==BrokerPacket.LOOKUP_REQUEST){
+					if(map.get(packetFromClient.symbol)==null){
+						packetToClient.symbol=packetFromClient.symbol+ " not found, try again";
+						System.out.println(packetFromClient.symbol+ " not found, try again");
+					}else{
+						ArrayList<BrokerLocation> list=map.get(packetFromClient.symbol);
+						Random rg = new Random();
+						packetToClient.symbol=list.get(rg.nextInt(list.size())).toString();
+						System.out.println(packetFromClient.symbol+" found: "+packetToClient.symbol);
+					}
+
+					/* send reply back to client */
+					packetToClient.type=BrokerPacket.LOOKUP_REPLY;
+					toClient.writeObject(packetToClient);
+					
+					/* wait for next packet */
+					continue;
+				}
+				
+				
+				
+			}
+			
+			
+			fromClient.close();
+			toClient.close();
+			socket.close();
+
+		} catch (IOException e) {
+			if(!gotByePacket)
+				e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			if(!gotByePacket)
+				e.printStackTrace();
+		}
+	}
+}
