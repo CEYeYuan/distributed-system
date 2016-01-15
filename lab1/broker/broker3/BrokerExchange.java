33a0c90ffca5d2299e622137fda60742c6dbdbb1
CEYeYuan <eceye.yuan@mail.utoronto.ca>
2016-01-15 12:57:29 -0500

    fixed the broker exchange server

--- lab1/broker/broker3/BrokerExchange.java
+++ lab1/broker/broker3/BrokerExchange.java
@@ -6,26 +6,60 @@ public class BrokerExchange{
 	public static void main(String[] args){
 		String host=null;
 		int port=-1;
+		String query=null;
 		ObjectOutputStream out = null;
 		ObjectInputStream in = null;
+		ObjectOutputStream out_lookup = null;
+		ObjectInputStream in_lookup = null;
+		String request=null;
 		try{
-			if(args.length == 2 ) {
+			if(args.length == 3 ) {
 				host=args[0];
 				port=Integer.parseInt(args[1]);
+				query=args[2];
 			} else {
 				System.err.println("ERROR: Invalid arguments!");
 				System.exit(-1);
 			}	
 			System.out.println("host: "+host+" port number: "+port);
-			Socket socket=new Socket(host, port);
+			Socket socket_lookup=new Socket(host, port);
 			Scanner scr = new Scanner(System.in);	
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		
-			out = new ObjectOutputStream(socket.getOutputStream());
-			in = new ObjectInputStream(socket.getInputStream());
 
+		
+			out_lookup = new ObjectOutputStream(socket_lookup.getOutputStream());
+			
+			System.out.println("Connecting to the default broker");
+
+			BrokerPacket packetToServer = new BrokerPacket();
+			packetToServer.type = BrokerPacket.LOOKUP_REQUEST;
+			packetToServer.symbol = args[2];
+			out_lookup.writeObject(packetToServer);
+
+			/* print server reply */
+			in_lookup = new ObjectInputStream(socket_lookup.getInputStream());
+			BrokerPacket packetFromServer;
+			packetFromServer = (BrokerPacket) in_lookup.readObject();
+			//System.out.println(packetFromServer.symbol);
+			if(packetFromServer.symbol.indexOf("not")!=-1){
+				System.out.println("not found, try it again");
+				System.exit(0);
+			}else{
+				int start=packetFromServer.symbol.indexOf(" HOST: ");
+				int end= packetFromServer.symbol.indexOf("PORT: ");
+				host=packetFromServer.symbol.substring(start+7,end);
+				port=Integer.parseInt(packetFromServer.symbol.substring(end+6));
+				System.out.println("Host: "+host+" Port: "+port);
+			}
+			
+			out_lookup.close();
+			in_lookup.close();
+			socket_lookup.close();
+			Socket socket=new Socket(host, port);
+			System.out.println("connected !");
+            out = new ObjectOutputStream(socket.getOutputStream());
+			in = new ObjectInputStream(socket.getInputStream());
 			System.out.print("CONSOLE>");	
-			String request=null;
 			while((request = scr.nextLine()) != null
 				&& request.toLowerCase().indexOf("bye") == -1){
 				/* make a new request packet */
@@ -39,7 +73,7 @@ public class BrokerExchange{
 				}
 				if(!(cmd[0].toLowerCase().equals("add")||cmd[0].toLowerCase().equals("update")||cmd[0].toLowerCase().equals("remove")))
 					continue;
-				BrokerPacket packetToServer = new BrokerPacket();
+				packetToServer = new BrokerPacket();
 				if(cmd[0].toLowerCase().equals("add")){
 					packetToServer.type = BrokerPacket.EXCHANGE_ADD;
 				}
@@ -56,7 +90,6 @@ public class BrokerExchange{
 				out.writeObject(packetToServer);
 
 				/* print server reply */
-				BrokerPacket packetFromServer;
 				packetFromServer = (BrokerPacket) in.readObject();
 
 				if (packetFromServer.type ==BrokerPacket.EXCHANGE_REPLY)
@@ -69,7 +102,7 @@ public class BrokerExchange{
 
 			}
 			/* tell server that i'm quitting */
-			BrokerPacket packetToServer = new BrokerPacket();
+			packetToServer = new BrokerPacket();
 			packetToServer.type = BrokerPacket.BROKER_BYE ;
 			packetToServer.symbol = "Bye!";
 			out.writeObject(packetToServer);
