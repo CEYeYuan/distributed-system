262231a13474c26f0437ad003fc24ea607f42126
CEYeYuan <eceye.yuan@mail.utoronto.ca>
2016-01-24 22:10:18 -0500

    also added seq number on client side

--- mazewar/partial/ClientSenderThread.java
+++ mazewar/partial/ClientSenderThread.java
@@ -16,10 +16,14 @@ public class ClientSenderThread implements Runnable {
     public void run() {
         MPacket toServer = null;
         if(Debug.debug) System.out.println("Starting ClientSenderThread");
+        int seq=0;
         while(true){
             try{                
                 //Take packet from queue
                 toServer = (MPacket)eventQueue.take();
+                //add a sequence number to make sure a client's move won't be out of order
+                toServer.sequenceNumber=seq;
+                seq++;
                 if(Debug.debug) System.out.println("Sending " + toServer);
                 mSocket.writeObject(toServer);    
             }catch(InterruptedException e){
--- mazewar/partial/ServerListenerThread.java
+++ mazewar/partial/ServerListenerThread.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.concurrent.BlockingQueue;
+import java.util.*;
 
 public class ServerListenerThread implements Runnable {
 
@@ -14,11 +15,29 @@ public class ServerListenerThread implements Runnable {
     public void run() {
         MPacket received = null;
         if(Debug.debug) System.out.println("Starting a listener");
+        //keep a local sequence number for each client thread
+        int seq=0;
+        PriorityQueue<MPacket> queue = new PriorityQueue<MPacket>(20,new Comparator<MPacket>() {
+            @Override
+            public int compare(MPacket p1, MPacket p2) {
+                return p1.sequenceNumber-p2.sequenceNumber;
+            }
+        });
         while(true){
             try{
                 received = (MPacket) mSocket.readObject();
+                queue.add(received);
                 if(Debug.debug) System.out.println("Received: " + received);
-                eventQueue.put(received);    
+                    while(!queue.isEmpty()){
+                        if(queue.peek().sequenceNumber==seq){
+                            received=queue.poll();
+                            eventQueue.put(received); 
+                            seq++;
+                        }
+                        else
+                            break;
+                    }
+                  
             }catch(InterruptedException e){
                 Thread.currentThread().interrupt();    
             }catch(IOException e){
