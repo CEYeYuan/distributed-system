80d0c6aa23c9c67e5a23f2cd952b3796b900f50d
CEYeYuan <eceye.yuan@mail.utoronto.ca>
2016-01-31 11:00:37 -0500

    used the latest version

--- mazewar/partial/MazeImpl.java
+++ mazewar/partial/MazeImpl.java
@@ -74,7 +74,10 @@ public class MazeImpl extends Maze implements Serializable, ClientListener, Runn
                 randomGen = new Random(seed);
                 
                 // Build the maze starting at the corner
-                buildMaze(new Point(0,0));
+                if(seed == 0)
+                    buildEmptyMaze();
+                else
+                    buildMaze(new Point(0,0));
 
                 thread.start();
         }
@@ -454,9 +457,9 @@ public class MazeImpl extends Maze implements Serializable, ClientListener, Runn
                 assert(client != null);
                 assert(checkBounds(point));
                 CellImpl cell = getCellImpl(point);
-                Direction d = Direction.random(randomGen);
+                Direction d = Direction.random();
                 while(cell.isWall(d)) {
-                  d = Direction.random(randomGen);
+                  d = Direction.random();
                 }
                 cell.setContents(client);
                 clientMap.put(client, new DirectedPoint(point, d));
@@ -489,20 +492,9 @@ public class MazeImpl extends Maze implements Serializable, ClientListener, Runn
                         point = new Point(randomGen.nextInt(maxX),randomGen.nextInt(maxY));
                         cell = getCellImpl(point);
                 }
-                
-                /***********************************************************
-                Every time, we 're creating a new Direction object, and what's 
-                worse is that we init a new randome generator that we didn't control
-                the seed ! So it's better to pass in a global random generator.
-                ************************************************************/
-
-
-
-
-
-                Direction d = Direction.random(randomGen);
+                Direction d = Direction.random();
                 while(cell.isWall(d)) {
-                        d = Direction.random(randomGen);
+                        d = Direction.random();
                 }
                 cell.setContents(target);
                 clientMap.put(target, new DirectedPoint(point, d));
@@ -887,6 +879,46 @@ public class MazeImpl extends Maze implements Serializable, ClientListener, Runn
                         buildMaze(newPoint);
                         d = pickNeighbor(point);
                 }
+        }
+        
+        /**
+         * Build a {@link Maze} with only
+         * the external boundaries.
+         */
+        private void buildEmptyMaze() {
+                //Start from the SW (0,0) corner
+                //iterate until NE(maxX, maxY) and remove all walls
+                //Remove all the interior walls
+                for(int x=1; x < maxX-1; x++){
+                    for(int y=1; y< maxY-1; y++){
+                        Point p = new Point(x,y);
+                        removeWall(p, Direction.North);
+                        removeWall(p, Direction.East);
+                        removeWall(p, Direction.South);
+                        removeWall(p, Direction.West);
+                    }
+                }
+                //Remove all the exterior walls
+                for(int x=1; x<maxX-1; x++){
+                    Point p = new Point(x,0);
+                    removeWall(p, Direction.East);
+                    removeWall(p, Direction.West);
+                    
+                    p = new Point(x, maxY-1);
+                    removeWall(p, Direction.East);
+                    removeWall(p, Direction.West);
+                }
+
+                for(int y=1; y<maxY-1; y++){
+                    Point p = new Point(0,y);
+                    removeWall(p, Direction.North);
+                    removeWall(p, Direction.South);
+                    
+                    p = new Point(maxX-1, y);
+                    removeWall(p, Direction.North);
+                    removeWall(p, Direction.South);
+                }
+                    
         }
        
         /** 
